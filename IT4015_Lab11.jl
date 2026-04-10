2acfa580cf762a735e2838da5f6623619cdfd8f9
iselekp <iselekp@mail.uc.edu>
2026-04-10 01:45:08 -0400

    tasks 1-3

---
+++ IT4015_Lab11.jl
@@ -0,0 +1,26 @@
+# IT 4015 Lab 11 - Introduction to Julia for Data Analysis | Karl Isele
+# Task 1: Your First Julia Script
+println("Hello, Julia world")
+
+# Task 2: Variables and Data Types
+print("What is your name? ")
+#name = readline()
+print("What is your age? ")
+#age = readline()
+print("What is your height in feet? ")
+#height = readline()
+
+#println("Name: ", name)
+#println("Age: ", age)
+#println("Height: ", height)
+
+println()
+
+# Task 3: Lists and Dictionaries
+favorite_foods = ["apple", "bean burritos", "banana", "pear", "tofu stir-fry", "pizza"]
+println(favorite_foods)
+
+movie_info = Dict("title" => "Redline", "director" => "Takeshi Koike", "year" => "2009")
+println(movie_info)
+
+# Task 4: Control Flow
